58a3b5ab1eab8736f72326dca4f237267ea73c51
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2020-05-12 19:35:58 +0430

    Unity Package Added

---
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float minSpeed = 12f;
+    public float maxSpeed = 16f;
+
+    public float xRange = 4f;
+    public float ySpawnPos = -6f;
+
+    private Rigidbody targetRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomUpwardForce(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector3 RandomUpwardForce()
+    {
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        return Vector3.up * randomSpeed;
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        float randomXPos = Random.Range(-xRange, xRange);
+        return new Vector3(randomXPos, ySpawnPos);
+    }
+
+
+
+
+}
